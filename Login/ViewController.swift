1ad33ebc00552e68ccc57839a4b24a0d9e34edff
Rich Castagna <idev.tru@gmail.com>
2018-12-08 19:16:28 -0500

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Richard Castagna on 12/8/18.
+//  Copyright © 2018 Richard Castagna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
