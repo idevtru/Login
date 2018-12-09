98741720cc974ad8f2915a2c06c110b1791eb384
Rich Castagna <idev.tru@gmail.com>
2018-12-08 21:27:13 -0500

    App Development with Swift, Lesson 3.6 Segues and Navigation Controllers

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var forgotUserNameButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func forgotUserName(_ sender: Any) {
+        performSegue(withIdentifier: "View To View", sender: forgotUserNameButton)
+    }
+    
+    @IBAction func forgotPassword(_ sender: Any) {
+        performSegue(withIdentifier: "View To View", sender: forgotPasswordButton)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
+        guard let sender = sender as? UIButton else {return}
+        
+        if sender == forgotPasswordButton {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else if sender == forgotUserNameButton {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = userNameTextField.text
+        }
+    }
 
 }
 
